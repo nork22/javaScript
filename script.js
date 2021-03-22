0cd90bcaf2c7d3a0a51b4216c0630498137afef6
nork22 <78579839+nork22@users.noreply.github.com>
2021-03-21 23:33:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+<script type="text/ javascript">
+
+        var loginForm = document.forms.login; // Or document.forms['login']
+        loginForm.elements.nome.placeholder = 'Norkis Reyes';
+        loginForm.elements.email.placeholder = 'f.28.01z@gmail.com';
+        loginForm.elements.password.placeholder = 'senha:12345';
+        loginform.elements.sexo.placeholder = 'sexo:F;M'
+        loginForm.elements.perfil.placeholder = 'perfil digite pelo menos 20 caracteres';
+        
+
+        </script>
